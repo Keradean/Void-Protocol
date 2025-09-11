14bce6018005ef35f8e9cd0cdbe8e41c993e0211
Dennis De Col <dennis.decol@gmx.net>
2025-09-11 15:56:12 +0200

    feat: Major weapon system overhaul with particles and UI improvements
    - Updated weapon system with integrated particle system
    - Added weapon reload functionality
    - Enhanced enemy hit detection with different particle effects
    - Started implementing player UI (work in progress)
    
    TODO: Complete remaining player UI elements

--- Assets/Scripts/Player/WeaponsController.cs
+++ Assets/Scripts/Player/WeaponsController.cs
@@ -5,7 +5,15 @@ public class WeaponsController : MonoBehaviour
     [Header("Confiq")]
     [SerializeField] private float range; 
     [SerializeField] private Transform mainCam; 
-    [SerializeField] private LayerMask validLayers; 
+    [SerializeField] private LayerMask validLayers;
+    [SerializeField] private GameObject impactEffect, damageEffect;
+    [SerializeField] private GameObject muzzelFlare;
+    [SerializeField] private float flareTime;
+    [SerializeField] private float flareCount;
+    [SerializeField] private int currentAmmo;
+    [SerializeField] private int clipSize;
+    [SerializeField] private int remainingAmmo;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,17 +23,58 @@ public class WeaponsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       if(flareCount > 0)
+        {
+          flareCount -= Time.deltaTime;
+            if(flareCount <= 0)
+            {
+                muzzelFlare.SetActive(false);
+            }
+        }
     }
 
     public void Shoot() 
     {
-        RaycastHit hit;
-        if (Physics.Raycast(mainCam.position, mainCam.forward, out hit, range, validLayers))
+        if (currentAmmo > 0)
         {
-            Debug.Log(hit.transform.name);
+            RaycastHit hit;
+            if (Physics.Raycast(mainCam.position, mainCam.forward, out hit, range, validLayers))
+            {
+                Debug.Log(hit.transform.name);
+
+                if (hit.transform.tag == "Enemy")
+                {
+                    Instantiate(damageEffect, hit.point, Quaternion.identity);
+                }
+                else
+                {
+
+                    Instantiate(impactEffect, hit.point, Quaternion.identity);
+
+                }
+            }
+            muzzelFlare.SetActive(true);
+            flareCount = flareTime;
+            currentAmmo--;
         }
-        
-        //Debug.Log("Sterbi!!");
+    }
+
+    public void Reload()
+    {
+        // Reload Logic 
+        remainingAmmo += currentAmmo;
+
+        if (remainingAmmo >= clipSize) 
+        { 
+        Debug.Log("I am Reloading my Weapon");
+        currentAmmo = clipSize;
+        remainingAmmo -= clipSize;
+        }
+        else
+        {
+            currentAmmo = remainingAmmo;
+            remainingAmmo = 0;
+        }
+        //PlayreloadAnimation(); // if its a littel bit Time lef´t
     }
 }
